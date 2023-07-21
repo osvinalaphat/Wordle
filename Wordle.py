74bbc72569a77261e33f1746ec7a838cad12104b
osvinalaphat <140211517+osvinalaphat@users.noreply.github.com>
2023-07-21 16:38:47 -0500

    Add files via upload

---
+++ Wordle.py
@@ -0,0 +1,31 @@
+answer = "gauze"
+tries = 6
+guess = None
+
+while guess != answer and tries != 0:
+
+    board = ["_", "_", "_", "_", "_",]
+    guess = str(input("Try to guess the word! "))
+
+    if len(guess) == 5:
+
+        tries = tries - 1
+
+        for i in range(len(guess)):
+
+            if guess[i] == answer[i]: # green tile
+                board[i] = guess[i]
+
+            elif guess[i] in answer: # yellow tile
+                board[i] = "🟡"
+                
+        print(''.join(board))
+
+    else:
+        print("Error: the word needs to be 5 letters")
+
+if guess == answer:
+    print("\nYou got the answer!")
+else:
+    print("\nYou lost.")
+print("The word was", answer)
